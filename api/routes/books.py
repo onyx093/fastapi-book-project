d8798d04525a9e629fc1feb8f4fabf84cd12056c
Olaleye Obidiya <olaleyeobidiya@gmail.com>
2025-02-12 12:19:45 +0100

    feat: add GET /api/v1/books/{book_id} endpoint
    1. Configures nginx server for reverse proxy
    2. Configures CI pipeline on pull request changes
    3. Configures CD pipeline for automatic deployment to Google Cloud Run

--- api/routes/books.py
+++ api/routes/books.py
@@ -1,6 +1,6 @@
 from typing import OrderedDict
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import JSONResponse
 
 from api.db.schemas import Book, Genre, InMemoryDB
@@ -47,6 +47,16 @@ async def create_book(book: Book):
 async def get_books() -> OrderedDict[int, Book]:
     return db.get_books()
 
+
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    for book in db.books.values():
+        if book.id == book_id:
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content=db.get_book(book_id).model_dump(),
+            )
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
 @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
 async def update_book(book_id: int, book: Book) -> Book:
